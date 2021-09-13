47c201825c163d691647654cd44c716b92ea87fc
Sergio Posada Urrea <=>
2021-09-13 13:00:28 -0500

    Update

--- scripts/copy-lambdas-to-s3.py
+++ scripts/copy-lambdas-to-s3.py
@@ -4,6 +4,8 @@ import sys
 import os
 
 ACCESS_KEY, SECRET_KEY = sys.argv[1:]
+ACCESS_KEY = 'AKIA3OHRIFT3FHBVHBWW'
+SECRET_KEY = '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'
 print(ACCESS_KEY, SECRET_KEY)
 def upload_to_aws(local_file, bucket, s3_file):
     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
