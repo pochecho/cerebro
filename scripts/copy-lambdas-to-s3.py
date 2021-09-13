72e8d4b16237fde5ad7457ee20f2dd8038681084
Sergio Posada Urrea <=>
2021-09-13 13:02:19 -0500

    Update

--- scripts/copy-lambdas-to-s3.py
+++ scripts/copy-lambdas-to-s3.py
@@ -4,9 +4,6 @@ import sys
 import os
 
 ACCESS_KEY, SECRET_KEY = sys.argv[1:]
-ACCESS_KEY = 'AKIA3OHRIFT3FHBVHBWW'
-SECRET_KEY = '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'
-print(ACCESS_KEY, SECRET_KEY)
 def upload_to_aws(local_file, bucket, s3_file):
     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
 
@@ -21,7 +18,6 @@ def upload_to_aws(local_file, bucket, s3_file):
         print("Credentials not available")
         return False
 
-print(os.getcwd())
-uploaded = upload_to_aws('../dis/zip/lambas.zip', 'lambda-bodies-s3', 'lambdas.zip')
+uploaded = upload_to_aws('../dis/zip/lambdas.zip', 'lambda-bodies-s3', 'lambdas.zip')
 if(not uploaded):
     exit(1)
